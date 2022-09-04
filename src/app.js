abd6d8cf41af9a0dec68a5c20ec08209f3f0f7b5
WilliamARivas <willarivas@yahoo.com>
2022-09-04 16:48:54 +0000

    my first excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const randomElement = array => {
+  let arrLength = array.length;
+  return array[Math.floor(Math.random() * arrLength)];
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.getElementById("excuse");
+  let newExcuse =
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when);
+  excuse.textContent = newExcuse;
 };
